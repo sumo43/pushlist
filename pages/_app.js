b5ada6cfd63093d69d89bef5b56de9b9e9e5550c
yatsenkoa <yatsenkoa2020@gmail.com>
2021-12-25 22:29:11 -0500

    added basic login flow with google, cleaned up site, added logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { ProvideAuth } from '../lib/auth';
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import customTheme from '../styles/theme';
+import { Global, css } from '@emotion/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider theme={customTheme}>
+			<ProvideAuth>
+				<CSSReset />
+				<Global
+					styles={css`
+						html {
+							min-width: 360px;
+							scroll-behavior: smooth;
+						}
+
+						#__next {
+							display: flex;
+							flex-direction: column;
+							min-height: 100vh;
+						}
+					`}
+				/>
+				<Component {...pageProps} />
+			</ProvideAuth>
+		</ThemeProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
